10e0bd22adea379ba58d13c39be461b8d99c9207
Muivovez <71385240+Muivovez@users.noreply.github.com>
2020-09-21 12:07:48 +0300

    kki
    k1

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	
+switch(3)
+{
+	case 1:
+		{
+		
+		std::cout<<1<<'\n';
+		system ("pause");
+		break;
+		
+		
+	}
+	case 2:
+		{
+		
+		std::cout<<2<<'\n';
+		break;
+	}
+	case 3:
+		{
+		
+		std::cout<<3<<'\n';
+		break;
+	}
+}
+	system ("pause");
+	return 0;
+}
